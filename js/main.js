6f4a0380a757688bfe1bdf5f325bdb5c36dbf426
Daniel McKenzie <d.mckenzie205@gmail.com>
2018-10-05 16:26:05 +0100

    responsive nav and map-container

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYXhvbG90bGRhbiIsImEiOiJjamxkdG9kcXUwZHI3M3FxZ29rcndwaGliIn0.NsxEQvM8xJD9k28SH7sW_Q',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
